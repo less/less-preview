18c9204269723def85c8356beda461f7eeafd64c
Luke Page <luke.a.page@gmail.com>
2015-06-09 16:24:52 +0100

    bind readonly

--- src/cmeditor.js
+++ src/cmeditor.js
@@ -1,15 +1,21 @@
+import {customElement, bindable} from 'aurelia-framework';
+
 import CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css!';
 import 'codemirror/theme/cobalt.css!';
 import 'codemirror/mode/css/css';
 
+@customElement("cmeditor")
 export class cmeditor {
+  @bindable readonly;
+
   attached() {
     CodeMirror.fromTextArea(this.cmTextarea, {
       lineNumbers: true,
       matchBrackets : true,
       mode: "text/x-less",
-      theme: "cobalt"
+      theme: "cobalt",
+      readOnly: this.readonly
     });
   }
 }
