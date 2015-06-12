384e3ce3b06e3df7d5d1e3b25532be71e74a15ba
Luke Page <luke.a.page@gmail.com>
2015-06-12 09:02:25 +0100

    less2css binding

--- src/cmeditor.js
+++ src/cmeditor.js
@@ -8,14 +8,26 @@ import 'codemirror/mode/css/css';
 @customElement("cmeditor")
 export class cmeditor {
   @bindable readonly;
+  @bindable value;
 
   attached() {
-    CodeMirror.fromTextArea(this.cmTextarea, {
+    this.codeMirror = CodeMirror.fromTextArea(this.cmTextarea, {
       lineNumbers: true,
       matchBrackets : true,
       mode: "text/x-less",
       theme: "cobalt",
       readOnly: this.readonly
     });
+    this.codeMirror.on('change', (codeMirror, changeObj) => {
+      const newValue = codeMirror.getValue();
+      if (newValue !== this.value) {
+        this.value = newValue;
+      }
+    });
+  }
+  valueChanged(newValue, oldValue) {
+    if (this.codeMirror && newValue !== this.codeMirror.getValue()) {
+      this.codeMirror.setValue(newValue);
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
+import less from './less';
+
 export class Main {
+  constructor() {
+    const lessVersions = less.getVersions();
+    less.loadVersion(lessVersions[lessVersions.length - 1]);
+  }
+  get lessSrc() {
+    return this._lessSrc;
+  }
+  set lessSrc(value) {
+    this._lessSrc = value;
+    less.convert(value)
+      .then((cssResp) => {
+        this.css = cssResp.css;
+      });
+  }
 }
