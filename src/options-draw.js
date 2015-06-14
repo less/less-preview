98545d7ea278db1d17344e0a43a7be7eb1b9b923
Luke Page <luke.a.page@gmail.com>
2015-06-14 10:32:22 +0100

    options slide-in/out

--- src/options-draw.js
+++ src/options-draw.js
@@ -5,4 +5,13 @@ export class optionsDraw {
   @bindable visible;
   attached() {
   }
+  visibleChanged(newValue) {
+    if (newValue) {
+      this.isDisplayed = true;
+      setTimeout(() => this.isVisible = true, 0);
+    } else {
+      this.isVisible = false;
+      setTimeout(() => this.isDisplayed = false, 1000);
+    }
+  }
 }
