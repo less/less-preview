fc8d31d714b31a6c66e0f176087e47820a4cec5c
Luke Page <luke.a.page@gmail.com>
2015-07-12 14:01:38 +0100

    Tidy up loading page.

--- build/tasks/less.js
+++ build/tasks/less.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 var less = require('gulp-less');
+var autoprefix = require('less-plugin-autoprefix');
+var cleancss = require('less-plugin-clean-css');
 
 gulp.task('less', function() {
   return gulp.src([paths.style])
-    .pipe(less())
+    .pipe(less({
+      plugins: [new autoprefix( { browsers: ["last 3 versions"] } ), new cleancss( { advanced: true} )]
+    }))
     .pipe(gulp.dest(paths.output))
 });
