bb361cc9d9dc35e05388884092869e8ecdbf67ec
Lei Chen <chenleicoder@gmail.com>
2022-07-09 22:12:07 +0800

    ci: set base path for deploy (#13)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'less-preview',
   build: {
     outDir: "docs",
     assetsInlineLimit: 0,
